554ac43b55c9ee769442a8eaf685626ad0fc2a17
MarcelMarin <marcelmarin256@gmail.com>
2021-05-17 10:19:08 -0400

    Aqui estoy usando el api de coincap para traerme a traves de rutas dinmicas un objeto con la informacion especifica de una criptomoneda, lo puedo comprobar yendo a /coin/bitcoin y entrar a la consola de vue y buenar el elemento asset para verificar esta data

--- src/api.js
+++ src/api.js
@@ -1,11 +1,18 @@
-const url = 'https://api.coincap.io/v2/assets?limit=25'
-
+const url = 'https://api.coincap.io/v2'
+//Esta es a funcion fetch q solicita a esa url que nos de la informacion solicitada
 function getAssets() {
-  return fetch(`${url}`)
+  return fetch(`${url}/assets?limit=25`)
+    .then((res) => res.json())
+    .then((res) => res.data)
+}
+
+function getAssetsCripto(coin) {
+  return fetch(`${url}/assets/${coin}`)
     .then((res) => res.json())
     .then((res) => res.data)
 }
 
 export default {
   getAssets,
+  getAssetsCripto,
 }
